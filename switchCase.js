fe9fa376d3da0d47fe9afd28d9ebe371249a616a
Sibu464 <74825943+Sibu464@users.noreply.github.com>
2024-04-04 11:42:45 +0200

    new things

---
+++ switchCase.js
@@ -0,0 +1,32 @@
+
+
+const prompt=require('prompt-sync')();// Library module for accepting user input
+let results;
+const operator=prompt('Enter either *,+,_,/ or: ');
+const num1=parseFloat(prompt('Enter a number'));
+const num2=parseFloat(prompt('Enter a number: '));
+switch (operator) {
+    case '*':
+      result = num1 * num2;
+        console.log("The result is: "+result);
+      break;
+    case '+':
+      result = num1 + num2;
+      console.log("The result is: "+result);
+      break;
+    case '_':
+      result = num1 - num2;
+      console.log("The result is: "+result);
+      break;
+    case '/':
+      if (num2!== 0) {
+        result = num1 / num2;
+        console.log(`The rsult it ${result}`);
+      } else {
+        console.log('Error: Division by zero is not allowed.');
+        break;
+      }
+    default:
+      console.log('Invalid operator. Please enter either *, +, -, or /.');
+      break;
+  }
