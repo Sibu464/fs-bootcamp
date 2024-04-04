d7cfa21be786ad34a35aa71648d58090a0b3e97b
Sibu464 <74825943+Sibu464@users.noreply.github.com>
2024-04-04 14:15:42 +0200

    arrays

---
+++ javascript/arrays.js
@@ -0,0 +1,10 @@
+
+let courses=["Angular","React","Vue","Java"];
+courses.length=10;//pre-defined array
+
+courses[4]="Typescript";
+console.log(courses.sort());
+courses.push("C++");
+console.log(courses.sort());
+console.log(courses.toString().toUpperCase());
+
