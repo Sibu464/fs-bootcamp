5d1a8be079e5b9d66fcbdafd2846c589051c3fe0
Sibu464 <74825943+Sibu464@users.noreply.github.com>
2024-04-04 11:50:07 +0200

    letter removed

--- switchCase.js
+++ switchCase.js
@@ -4,7 +4,7 @@ const prompt=require('prompt-sync')();// Library module for accepting user input
 let results;
 const operator=prompt('Enter either *,+,_,/ or: ');
 const num1=parseFloat(prompt('Enter a number'));
-const num2=parseFloat(prompt('Enter a number: '));
+const num2=parseFloat(prompt('Enter a nmber: '));
 switch (operator) {
     case '*':
       result = num1 * num2;
